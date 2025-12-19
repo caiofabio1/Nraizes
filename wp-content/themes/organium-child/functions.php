59a054775ad74fb3e41a659c97400365bb62dba5
caiofabio1 <caiofabio1@gmail.com>
2025-12-19 13:04:59 -0300

    Initial commit: Organium child theme with CI/CD workflow

---
+++ wp-content/themes/organium-child/functions.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Organium Child Theme Functions
+ * 
+ * @package Organium-Child
+ * @version 1.1.0
+ */
+
+add_action( 'wp_enqueue_scripts', 'enqueue_theme_styles' );
+function enqueue_theme_styles() {
+    if (class_exists('WooCommerce')) {
+        wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array('organium-theme', 'organium-style', 'organium-woocommerce') );
+    } else {
+        wp_enqueue_style( 'child-style', get_stylesheet_directory_uri() . '/style.css', array('organium-theme', 'organium-style') );
+    }
+}
+
+// ============================================
+// Adicione suas customizações abaixo
+// ============================================
